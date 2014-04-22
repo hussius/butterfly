90a8adf72f37916c36190856117a642b7bc7e264
Johan Reimegård <johan.reimegard@gmail.com>
2014-04-22 16:00:32 +0200

    An example shell script to start the scripts

---
+++ runAnalysis.sh
@@ -0,0 +1,10 @@
+./topGOanalysisEdgeR.R data/Hmel1.21.cdhit.ARGOT2.GOannotation.txt  argot2 data/fatbody_vs_other_tissues.txt  0.05 
+./topGOanalysisEdgeR.R data/Hmel1.21.cdhit.ARGOT2.GOannotation.txt  argot2 data/labialgland_vs_othertissues.txt 0.05  
+./topGOanalysisEdgeR.R data/Hmel1.21.cdhit.ARGOT2.GOannotation.txt  argot2 data/fatbody_vs_other_tissues.txt 0.05  
+./topGOanalysisEdgeR.R data/Hmel1.21.cdhit.ARGOT2.GOannotation.txt  argot2 data/malpighiantubules_vs_other_tissues.txt 0.05    
+
+./topGOanalysisEdgeR.R data/Hmel1.21.cdhit.blast2go.GOannotation.txt  blast2GO data/fatbody_vs_other_tissues.txt  0.05 
+./topGOanalysisEdgeR.R data/Hmel1.21.cdhit.blast2go.GOannotation.txt  blast2GO data/labialgland_vs_othertissues.txt 0.05  
+./topGOanalysisEdgeR.R data/Hmel1.21.cdhit.blast2go.GOannotation.txt  blast2GO data/fatbody_vs_other_tissues.txt 0.05  
+./topGOanalysisEdgeR.R data/Hmel1.21.cdhit.blast2go.GOannotation.txt  blast2GO data/malpighiantubules_vs_other_tissues.txt 0.05    
+
